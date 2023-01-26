147fb7200d051bd1101ff7a31b9b38314fe009b3
jordi-fontan <63612112+jordi-fontan@users.noreply.github.com>
2023-01-26 16:24:19 +0100

    Add Lab2,testMethods, Graded Test

---
+++ 6-JUnit/Labs/test/com/testing/lang/StringTest.java
@@ -0,0 +1,65 @@
+package com.testing.lang;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.function.Supplier;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+@DisplayName("Mi awesome Test Suite")
+class StringTest {
+	String myString;
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		myString="12345678";
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	@DisplayName("Test the Length of a String")
+	final void testLength() {
+		System.out.println("Testing length " + myString);
+		assertTrue(8==myString.length());
+		assertFalse(9==myString.length());
+	}
+
+	@Test
+	final void testSubstring() {
+		System.out.println("Testing substring " + myString);
+		System.out.println("Testing substring " + myString.substring(0,4));
+		
+		assertEquals("1234",myString.substring(0,4));
+		assertNotEquals("1235",myString.substring(0,4));
+		Supplier<String> messageSupplier  = () -> "Substring 0,4 test failed";
+		//make it fail to check supplier
+		System.out.println("Testing substring message " + messageSupplier.get());
+		assertNotEquals("5", myString.substring(0,4), messageSupplier);
+		
+	}
+
+	/*
+	@Test
+	void testCase() {
+
+		Throwable exception = assertThrows(NullPointerException.class, () -> {
+	        throw new IllegalArgumentException("error message");
+	    });
+	}
+	*/
+}
