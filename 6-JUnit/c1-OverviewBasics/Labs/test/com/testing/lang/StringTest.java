326eea20581b61d3d0c770c806e0c70356d3cf2d
jordi-fontan <63612112+jordi-fontan@users.noreply.github.com>
2023-01-27 07:08:52 +0100

    Add comments

--- 6-JUnit/c1-OverviewBasics/Labs/test/com/testing/lang/StringTest.java
+++ 6-JUnit/c1-OverviewBasics/Labs/test/com/testing/lang/StringTest.java
@@ -8,8 +8,14 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+/*
+ *  Class showing the basics of JUnit
+ *  Also includes @Displayname and Supplier func Interface
+ */
+@DisplayName("Testing Java Strings!!")
 class StringTest {
 	String myString;
 	@BeforeAll
@@ -31,19 +37,32 @@ class StringTest {
 	}
 
 	@Test
+	@DisplayName("String length Test")
 	final void testLength() {
-		System.out.println("Testing length " + myString);
+		
+		// Trace length test to check test order
+		System.out.println("Testing length " + myString); 
+		
+		// Check for succeed and fail
 		assertTrue(8==myString.length());
 		assertFalse(9==myString.length());
 	}
 
 	@Test
+	@DisplayName("SubString Test")
 	final void testSubstring() {
+		
+		// Trace substring test
 		System.out.println("Testing substring " + myString);
 		System.out.println("Testing substring " + myString.substring(0,4));
 		
+		// Check for succeed and fail
+		
 		assertEquals("1234",myString.substring(0,4));
 		assertNotEquals("1235",myString.substring(0,4));
+		
+		// Testing Supplier lambda
+		
 		Supplier<String> messageSupplier  = () -> "Substring 0,4 test failed";
 		//make it fail to check supplier
 		System.out.println("Testing substring message " + messageSupplier.get());
